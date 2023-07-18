01d59bd7877281c1a7cd97d6439ebde0d02c8d47
wjdehrbs <rhtmxhavus@naver.com>
2023-07-18 19:40:03 +0900

    fix

--- app/src/main/java/com/example/calculator/calculator.kt
+++ app/src/main/java/com/example/calculator/calculator.kt
@@ -1,30 +1,33 @@
-package com.example.calculator
-
 import java.util.Scanner
 
+class Calculator {
+    fun calculate(num1: Double, num2: Double, operator: String): Double {
+        return when (operator) {
+            "+" -> num1 + num2
+            "-" -> num1 - num2
+            "*" -> num1 * num2
+            "/" -> num1 / num2
+            else -> throw IllegalArgumentException("실패!")
+        }
+    }
+}
+
 fun main() {
-    val calculator = Scanner(System.`in`)
+    val scanner = Scanner(System.`in`)
 
     println("사칙연산 계산기")
 
     print("첫 번째 숫자를 입력하세요: ")
-    val num1 = calculator.nextDouble()
+    val num1 = scanner.nextDouble()
 
     print("두 번째 숫자를 입력하세요: ")
-    val num2 = calculator.nextDouble()
+    val num2 = scanner.nextDouble()
 
-    print("연산자를 선택하세요 (+, -, *, / , %): ")
-    val operator = calculator.next()
+    print("연산자를 선택하세요 (+, -, *, /): ")
+    val operator = scanner.next()
 
-    val result = when (operator) {
-        "+" -> num1 + num2
-        "-" -> num1 - num2
-        "*" -> num1 * num2
-        "/" -> num1 / num2
-        "%" -> num1 / num2
+    val calculator = Calculator()
+    val result = calculator.calculate(num1, num2, operator)
 
-        else -> throw IllegalArgumentException("실패")
-    }
-
-    println("$num1 과 $num2 의 $operator 결과는 $result 입니다")
+    println("$num1 와 $num2 의 $operator 한 결과는 $result 입니다")
 }
